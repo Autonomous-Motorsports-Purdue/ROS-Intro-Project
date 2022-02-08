e38cdce4b33a38953ac166fff62f7df1764d32fa
niklaus-g <nmyrick@purdue.edu>
2022-02-08 02:36:35 -0500

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nick/intro-project/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/nick/intro-project/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/nick/intro-project/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/nick/intro-project/src:$ROS_PACKAGE_PATH"
