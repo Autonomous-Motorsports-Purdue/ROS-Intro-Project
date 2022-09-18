33064ff6b53c419937b1a07bb8ce1beaf401627d
Hayagreev <hayagreev.dilip@gmail.com>
2022-09-18 15:52:14 +0000

    initial commit

---
+++ intro-project/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hayagreev/ROS-Intro-Project/intro-project/src
+# Build directory: /home/hayagreev/ROS-Intro-Project/intro-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ intro-project/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/hayagreev/catkin_ws/devel/lib:/opt/ros/noetic/lib'
+export PWD='/home/hayagreev/ROS-Intro-Project/intro-project/build'
+export ROS_PACKAGE_PATH='/home/hayagreev/ROS-Intro-Project/intro-project/src:/home/hayagreev/catkin_ws/src:/opt/ros/noetic/share'
